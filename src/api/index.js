f3df71f53c29d6fd7d5fdd61e567b809546b8b48
chuckwhisler <usmanmohid@gmail.com>
2024-01-21 07:02:46 +0500

    fix(api)

--- src/api/index.js
+++ src/api/index.js
@@ -135,31 +135,44 @@ app.post('/api/video/duration/get', async (req, res) => {
 });
 
 app.post('/api/video/convert/audio', (req, res) => {
-    let data = req.body.data;
+    let data = JSON.parse(req.body.data);
     let key = req.body.key;
     let account_id = req.body.account_id;
 
-    console.log(req.body);
-    // const audioFiles = [];
-    // const delays = [];
-    // for (var audio of data.transcription) {
-    //     audioFiles.push(`/usr/share/nginx/html/source/${account_id}/${key}_${audio.start_time}_${audio.end_time}_translated_audio.mp3`);
-    //     delays.push(audio.start_time * 1000)
-    // }
-    //
-    // const audioInputs = audioFiles.map((file, index) => `[${index + 1}:a]adelay=${delays[index]}[a${index + 1}]`).join(';');
-    // const audioMix = audioFiles.map((_, index) => `[a${index + 1}]`).join('');
-    //
-    // Ffmpeg()
-    //     .input(`/usr/share/nginx/html/source/${account_id}/${key}.mp4`)
-    //     .input(audioFiles.map((file, index) => `-i ${file}`).join(' '))
-    //     .complexFilter(`${audioInputs};${audioMix}amix=inputs=${audioFiles.length}[aout]`)
-    //     .audioCodec('aac')
-    //     .videoCodec('copy')
-    //     .on('end', () => console.log('Processing finished'))
-    //     .on('error', (err) => console.error('Error:', err))
-    //     .save(`/usr/share/nginx/html/source/${account_id}/${key}_output.mp4`);
-    //
+    const audioFiles = [];
+    const delays = [];
+    for (var audio of data.transcription) {
+        // audioFiles.push(`/home/shayannadeem/Downloads/audio_segments/${key}_${audio.start_time}_${audio.end_time}_translated_audio.mp3`);
+        audioFiles.push(`/usr/share/nginx/html/source/${account_id}/${key}_${audio.start_time}_${audio.end_time}_translated_audio.mp3`);
+        delays.push(audio.start_time * 1000)
+    }
+
+    const audioInputs = audioFiles.map((file, index) => `[${index + 1}:a]adelay=${delays[index]}[a${index + 1}]`).join(';');
+    const audioMix = audioFiles.map((_, index) => `[a${index + 1}]`).join('');
+
+    console.log(audioInputs);
+    console.log(audioMix);
+    console.log(delays);
+    console.log(audioFiles);
+
+    let ffmpeg = Ffmpeg();
+    ffmpeg.input(`/usr/share/nginx/html/source/${account_id}/${key}_no_audio.mp4`);
+
+    audioFiles.forEach((input) => {
+        ffmpeg.input(input);
+    });
+
+    console.log(`${audioInputs};${audioMix}amix=inputs=${audioFiles.length}[aout]`)
+
+    ffmpeg
+        .complexFilter(`${audioInputs};${audioMix}amix=inputs=${audioFiles.length}[aout]`)
+        .audioCodec('aac')
+        .videoCodec('copy')
+        .outputOptions(["-map 0:v", "-map [aout]", "-c:v copy", "-c:a aac"])
+        .on('end', () => console.log('Processing finished'))
+        .on('error', (err) => console.error('Error:', err))
+        .save(`/usr/share/nginx/html/source/${account_id}/${key}_output.mp4`);
+
     res.json({ message: "Video Completed" });
 })
 
