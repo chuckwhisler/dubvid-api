4b2e84bbf54c92c254eda3a57910641352378abe
chuckwhisler <usmanmohid@gmail.com>
2024-01-21 20:07:02 +0500

    fix(api)

--- src/api/index.js
+++ src/api/index.js
@@ -13,57 +13,69 @@ app.listen("3001", () => {
     console.log("Listening on 3001");
 });
 
-app.post('/api/video/convert', (req, res) => {
-    let data = JSON.parse(req.body.data);
-    let key = req.body.key;
-    let account_id = req.body.account_id;
-
-    const audioFiles = [];
-    const delays = [];
-    for (var audio of data.transcription) {
-        // audioFiles.push(`/home/shayannadeem/Downloads/audio_segments/${key}_${audio.start_time}_${audio.end_time}_translated_audio.mp3`);
-        audioFiles.push(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_${audio.start_time}_${audio.end_time}_translated_audio.mp3`);
-        delays.push(audio.start_time * 1000)
-    }
-
-    const audioInputs = audioFiles.map((file, index) => `[${index + 1}:a]adelay=${delays[index]}[a${index + 1}]`).join(';');
-    const audioMix = audioFiles.map((_, index) => `[a${index + 1}]`).join('');
-
-    console.log(audioInputs);
-    console.log(audioMix);
-    console.log(delays);
-    console.log(audioFiles);
-    console.log("account_id:", account_id);
-    console.log("key:", key);
-
-    Ffmpeg(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}.mp4`)
-        .noAudio()
-        .on('end', () => console.log('Audio Removed finished'))
-        .on('error', (err) => console.error('Audio Error:', err))
-        .saveToFile(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_no_audio.mp4`);
-
-    setTimeout(() => {
-        let ffmpeg = Ffmpeg();
-        ffmpeg.input(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_no_audio.mp4`);
-
-        audioFiles.forEach((input) => {
-            ffmpeg.input(input);
-        });
-
-        console.log(`${audioInputs};${audioMix}amix=inputs=${audioFiles.length}[aout]`)
-
-        ffmpeg
-            .complexFilter(`${audioInputs};${audioMix}amix=inputs=${audioFiles.length}[aout]`)
-            .audioCodec('aac')
-            .videoCodec('copy')
-            .outputOptions(["-map 0:v", "-map [aout]", "-c:v copy", "-c:a aac"])
-            .on('end', () => console.log('Processing finished'))
-            .on('error', (err) => console.error('Error:', err))
-            .saveToFile(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_output.mp4`);
-
-        res.json({ message: "Video Completed" });
-    }, 10000);
-})
+// app.post('/api/video/dubbing', (req, res) => {
+//     let key = req.body.key;
+//     let account_id = req.body.account_id;
+//
+//     Ffmpeg(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}.mp4`)
+//         .noAudio()
+//         .on('end', () => console.log('Audio Removed finished'))
+//         .on('error', (err) => console.error('Audio Error:', err))
+//         .saveToFile(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_no_audio.mp4`);
+//
+//     res.json({ message: "Video Completed" });
+// })
+// app.post('/api/video/convert', (req, res) => {
+//     let data = JSON.parse(req.body.data);
+//     let key = req.body.key;
+//     let account_id = req.body.account_id;
+//
+//     const audioFiles = [];
+//     const delays = [];
+//     for (var audio of data.transcription) {
+//         // audioFiles.push(`/home/shayannadeem/Downloads/audio_segments/${key}_${audio.start_time}_${audio.end_time}_translated_audio.mp3`);
+//         audioFiles.push(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_${audio.start_time}_${audio.end_time}_translated_audio.mp3`);
+//         delays.push(audio.start_time * 1000)
+//     }
+//
+//     const audioInputs = audioFiles.map((file, index) => `[${index + 1}:a]adelay=${delays[index]}[a${index + 1}]`).join(';');
+//     const audioMix = audioFiles.map((_, index) => `[a${index + 1}]`).join('');
+//
+//     console.log(audioInputs);
+//     console.log(audioMix);
+//     console.log(delays);
+//     console.log(audioFiles);
+//     console.log("account_id:", account_id);
+//     console.log("key:", key);
+//
+//     Ffmpeg(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}.mp4`)
+//         .noAudio()
+//         .on('end', () => console.log('Audio Removed finished'))
+//         .on('error', (err) => console.error('Audio Error:', err))
+//         .saveToFile(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_no_audio.mp4`);
+//
+//     setTimeout(() => {
+//         let ffmpeg = Ffmpeg();
+//         ffmpeg.input(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_no_audio.mp4`);
+//
+//         audioFiles.forEach((input) => {
+//             ffmpeg.input(input);
+//         });
+//
+//         console.log(`${audioInputs};${audioMix}amix=inputs=${audioFiles.length}[aout]`)
+//
+//         ffmpeg
+//             .complexFilter(`${audioInputs};${audioMix}amix=inputs=${audioFiles.length}[aout]`)
+//             .audioCodec('aac')
+//             .videoCodec('copy')
+//             .outputOptions(["-map 0:v", "-map [aout]", "-c:v copy", "-c:a aac"])
+//             .on('end', () => console.log('Processing finished'))
+//             .on('error', (err) => console.error('Error:', err))
+//             .saveToFile(`/usr/share/nginx/html/source/public/videos/${account_id}/${key}_output.mp4`);
+//
+//         res.json({ message: "Video Completed" });
+//     }, 10000);
+// })
 
 app.post('/api/video/create/thumbnail', (req, res) => {
     console.log(req.body.video_path);
@@ -89,29 +101,28 @@ app.post('/api/video/duration/get', async (req, res) => {
 });
 
 
-// app.post('/api/video/convert', (req, res) => {
-//     console.log(req.body)
-//     Ffmpeg(`/usr/share/nginx/html/source/${req.body.video_path}`)
-//         .inputOptions(['-async', '1'])
-//         .on('end', () => {
-//             Ffmpeg()
-//                 .input(`/usr/share/nginx/html/source/${req.body.video_path.split('.')[0] + "_no_audio." + req.body.video_path.split('.')[1]}`)
-//                 .videoCodec('copy')
-//                 .input(`/usr/share/nginx/html/source/${req.body.audio_path}`)
-//                 .on('end', () => {
-//                     console.log('Conversion finished!');
-//                 })
-//                 .on('error', (err) => {
-//                     console.error('Error:', err);
-//                 })
-//                 .saveToFile(`/usr/share/nginx/html/source/${req.body.output_path}`);
-//             res.json({ "message": "File converted successfully." });
-//             console.log("completed")
-//         }).on('error', (e) => {
-//             console.log("err", e)
-//         })
-//         .noAudio()
-//         .saveToFile(`/usr/share/nginx/html/source/${req.body.video_path.split('.')[0] + "_no_audio." + req.body.video_path.split('.')[1]}`);
-// });
+app.post('/api/video/convert', (req, res) => {
+    console.log(req.body)
+    Ffmpeg(`/usr/share/nginx/html/source/${req.body.video_path}`)
+        .on('end', () => {
+            Ffmpeg()
+                .input(`/usr/share/nginx/html/source/${req.body.video_path.split('.')[0] + "_no_audio." + req.body.video_path.split('.')[1]}`)
+                .videoCodec('copy')
+                .input(`/usr/share/nginx/html/source/${req.body.audio_path}`)
+                .on('end', () => {
+                    console.log('Conversion finished!');
+                })
+                .on('error', (err) => {
+                    console.error('Error:', err);
+                })
+                .saveToFile(`/usr/share/nginx/html/source/${req.body.output_path}`);
+            res.json({ "message": "File converted successfully." });
+            console.log("completed")
+        }).on('error', (e) => {
+            console.log("err", e)
+        })
+        .noAudio()
+        .saveToFile(`/usr/share/nginx/html/source/${req.body.video_path.split('.')[0] + "_no_audio." + req.body.video_path.split('.')[1]}`);
+});
 
 export default app;
