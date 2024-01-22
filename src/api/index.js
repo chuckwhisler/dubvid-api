10181460513f436e7b5778ffd7ac60e3ff281652
chuckwhisler <usmanmohid@gmail.com>
2024-01-22 23:51:36 +0500

    fix(api)

--- src/api/index.js
+++ src/api/index.js
@@ -144,8 +144,8 @@ app.post('/api/video/convert', (req, res) => {
 });
 
 app.post('/api/video/compress', (req, res) => {
-    console.log(video_path);
     let video_path = `/usr/share/nginx/html/source/${req.body.video_path}`;;
+    console.log(video_path);
     let outputPath = `/usr/share/nginx/html/source/${req.body.video_path.split('.')[0] + "_compressed." + req.body.video_path.split('.')[1]}`;
     Ffmpeg()
         .input(video_path)
